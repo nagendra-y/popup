d0f78e36298ead1e926525d7ef52d0c6ebd44fa8
Nagendra <contact@nagendrawhy.com>
2020-11-24 18:56:52 +0530

    x

--- backup.sh
+++
@@ -1,3 +0,0 @@
-BACKUPFILE=../backup/messenger-javascript-$(date +%d%b%y-%H%M).bz2
-tar --exclude='./backup.sh' --exclude='./closurecheck.sh' --exclude='./serve' -jcvf $BACKUPFILE .
-
--- closurecheck.sh
+++
@@ -1,2 +0,0 @@
-java -jar /home/mesibo/closure-compiler-v20191111.jar --compilation_level ADVANCED_OPTIMIZATIONS --js $1 
-
